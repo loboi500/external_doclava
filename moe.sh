7f5f48a414b3f9a48e8e4806407180eee0281abe
Trilokesh Rangam <rangam.lokesh@gmail.com>
2023-01-02 22:44:29 +0530

    lfs tracking enabled
    Signed-off-by: Trilokesh Rangam <rangam.lokesh@gmail.com>

--- moe.sh
+++ moe.sh
@@ -1,23 +1,3 @@
-#!/bin/bash
-
-# Copyright (C) 2010 The Android Open Source Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# A simple script to import the latest code for Doclava and repackage it
-# for the Android build system. Repackaging involves:
-#   * Removing the lib/ directory- jsilver is built from source, and
-#     ant is not used.
-#   * Removing the ant DoclavaTask task (so we don't require ant to build)
-
-rm -fr res/ src/ && svn export --force https://doclava.googlecode.com/svn/trunk/ . && rm -fr lib/ samples/ src/com/google/doclava/DoclavaTask.java
+version https://git-lfs.github.com/spec/v1
+oid sha256:3f8580c65646daf49418c8a06a08d2cae22c776ad59e973a49bc9c6a027b93ac
+size 1056
